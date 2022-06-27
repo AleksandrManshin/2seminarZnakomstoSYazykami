ace901053e7b6fb179077d549e76861c45070abc
Aleksandr <maivan007@yandex.ru>
2022-06-27 22:08:40 +0400

    Add Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rand = new Random();
+int number = rand.Next(10, 100);
+Zadacha9(number);
+number = rand.Next(10, 100);
+//Console.WriteLine(number);
+
+//int digitFirst = number/10;
+//int digitSecond = number%10;
+//if(digitFirst > digitSecond)
+//{
+//    Console.WriteLine(digitFirst);
+//}
+//else if(digitFirst < digitSecond)
+//{
+//    Console.WriteLine(digitSecond);
+//}
+//else
+//{
+   // Console.WriteLine("Цифры числа равны");
+//}
+
+
+void Zadacha9(int currentNumber) //f(x)
+{
+  Console.WriteLine(currentNumber);
+ int digitFirst = currentNumber/10;
+ int digitSecond = currentNumber%10;
+ if(digitFirst > digitSecond)
+  Console.WriteLine(digitFirst);
+  else if(digitFirst < digitSecond)
+  Console.WriteLine(digitSecond);
+  else
+   Console.WriteLine("Цифры числа равны");
+}
