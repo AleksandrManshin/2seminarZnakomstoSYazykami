8604242b5e3cd9f802784626f74da17e49d567f8
Aleksandr <maivan007@yandex.ru>
2022-07-02 10:56:24 +0400

    Add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+
+TripleNumber(number);
+
+void TripleNumber(string number)
+{
+    //Console.WriteLine(number);
+    Console.Write(number[0]);
+    Console.Write(number[2]);
+    Console.WriteLine();
+}
