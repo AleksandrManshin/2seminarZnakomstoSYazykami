49ae350d37fda5e143b7e766a0a84baf852ff6d5
Aleksandr <maivan007@yandex.ru>
2022-07-03 21:29:51 +0400

    Add Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿void Zadacha14(int numb)
+{
+    if(numb % 7 == 0)
+    {
+        if(numb % 23 == 0)
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    } 
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+Console.WriteLine("Введите число, и я скажу явлеяется ли оно одновременно кратно 7 и 23:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Zadacha14(number);
